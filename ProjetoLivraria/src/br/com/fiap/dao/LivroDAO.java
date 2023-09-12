dd73d9207bf77bf8d474c06ead01824a56d828ea
Gabriel <106111030+gabrielrodri33@users.noreply.github.com>
2023-09-12 11:13:23 -0300

    Projeto que conectamos com banco e falta o delete

---
+++ ProjetoLivraria/src/br/com/fiap/dao/LivroDAO.java
@@ -0,0 +1,34 @@
+package br.com.fiap.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import br.com.fiap.beans.Livro;
+import br.com.fiap.conexoes.ConexaoFactory;
+
+public class LivroDAO {
+	
+	public Connection minhaConexao;
+	
+	public LivroDAO() throws ClassNotFoundException, SQLException {
+		super();
+		this.minhaConexao = new ConexaoFactory().conexao();
+	}
+	
+	//insert
+	public String inserir (Livro livro) throws SQLException {
+		PreparedStatement stmt = minhaConexao.prepareStatement("INSERT INTO t_fiap_livro values(?, ?, ?, ?, ?, ?)");
+		stmt.setInt(1, livro.getCodigo());
+		stmt.setString(2, livro.getTitulo());
+		stmt.setString(3, livro.getEditora());
+		stmt.setString(4, livro.getAutor());
+		stmt.setInt(5, livro.getQuantidade());
+		stmt.setDouble(6, livro.getValor());
+		
+		stmt.execute();
+		stmt.close();
+		return "Cadastrado com sucesso!";
+	}
+}
+
---
+++ ProjetoLivraria/src/br/com/fiap/main/TesteCadastro.java
@@ -0,0 +1,10 @@
+package br.com.fiap.main;
+
+public class TesteCadastro {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
